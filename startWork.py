75b15500f42f8577f6ff8d91b915822408d298bd
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2023-08-22 02:42:22 -0700

    Update startWork.py

--- startWork.py
+++ startWork.py
@@ -1,23 +1,21 @@
 import webbrowser
 import subprocess
 import time
+import questionary
+
+# Define the user name to be used in the directories
+userName = 'Peter'
 
 def open_apps_and_urls():
     # Define the URLs to be opened in Google Chrome
     urls = [
         'https://gitlab.com/dashboard/merge_requests?assignee_username=petervillaroman',
-        'https://clickup.com',
+        'https://app.clickup.com/?_ga=2.67863057.140528526.1661975994-1432757507.1661975994&_gl=1*1t9phd1*_gcl_au*MTY3NzI1Njk0Ni4xNjg1NTY0Mjc1',
         'https://chat.openai.com',
         'https://bit.ly/clockNellis'
     ]
-
-    # Open the URLs in the default browser (in separate tabs)
-    first_url = urls.pop(0)
-    webbrowser.open_new(first_url)
-    time.sleep(1)  # to make sure the first browser window has time to open
-    for url in urls:
-        webbrowser.open_new_tab(url)
-        
+    # Defining the applications to be opened
+    
     applications = [ 
         'Slack', 
         'GitHub Desktop',
@@ -26,19 +24,36 @@ def open_apps_and_urls():
     # Define the directories to be opened in Visual Studio Code
     directories = [
         # '/users/Peter/documents/nellis-files/buyer', disabled for now bc I'm not working on buyer atm
-        '/users/Peter/documents/nellis-files/cargo',
-        '/users/Peter/documents/nellis-files/cargo-api',
-        # Add more directories as needed
+        f'/users/{userName}/documents/nellis-files/cargo',
+        f'/users/{userName}/documents/nellis-files/cargo-api',
     ]
 
+    ide_choice = questionary.select(
+        "Which IDE would you like to use?",
+        choices=[
+            "VSCode",
+            "IntelliJ"
+        ]
+    ).ask()
 
+        
+    # Open the VSCode directories
+    for directory in directories:
+        if ide_choice == "IntelliJ":
+            subprocess.Popen(['idea', directory])
+        else:
+            subprocess.Popen(['open', '-na', 'Visual Studio Code', '--args', directory])
+
+    # Open the URLs in the default browser (in separate tabs)
+    first_url = urls.pop(0)
+    webbrowser.open_new(first_url)
+    time.sleep(1)  # to make sure the first browser window has time to open
+    for url in urls:
+        webbrowser.open_new_tab(url)   
+
+    # Open the applications
     for application in applications:
         subprocess.Popen(['open', '-na', application])
-
-    # Open the directories in new Visual Studio Code windows
-    for directory in directories:
-        subprocess.Popen(['open', '-na', 'Visual Studio Code', '--args', directory])
-
 
 
 
